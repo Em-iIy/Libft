f2ef4d088f0ce629dbce691dbdabf8e5e2597c8b
Glenn Winnink <gwinnink@student.codam.nl>
2022-02-23 10:18:11 +0100

    fixed bad input checks

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 13:44:54 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/02/22 18:09:45 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/02/23 10:16:33 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	unsigned int	i;
 
 	i = 0;
+	if (len > ft_strlen(s))
+		len = ft_strlen(s) - start;
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
 	ret = (char *)malloc((len + 1) * sizeof(char));
 	if (!ret)
 		return (NULL);
-	while (i < len && s[start + i] && start < ft_strlen(s))
+	while (i < len && s[start + i])
 	{
 		ret[i] = s[start + i];
 		i++;
